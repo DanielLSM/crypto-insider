6ede30b9895527355902a0dc87fe5605cba60a0b
Daniel Marta <daniellsmarta@gmail.com>
2018-02-18 18:12:34 +0000

    scrapper changes

--- crypto-insider/scrapper.py
+++ crypto-insider/scrapper.py
@@ -2,18 +2,19 @@ from lxml import html
 import requests
 
 # https://cryptoping.tech/
+# http://docs.python-guide.org/en/latest/scenarios/scrape/
 
 DEFAULT = {'Cointelegraph' : 'https://cointelegraph.com/'}
                     # 'zerohedge' : ''}
 
 #region scrapper functions
-def tree_from_html(url):
+def tree_from_html(url: str):
     page = requests.get(url)
     return html.fromstring(page.content)
 
 def forest(trees: dict):
     pass
-    
+
 
 
 
